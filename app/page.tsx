b823c1afe0ec3c22530aef1d1180930c0f21eded
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-03-16 11:24:21 +0530

    Started next app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center bg-sky-200 text-gray-950">
+      Homepage
+    </div>
+  );
+}
