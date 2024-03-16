b2a1e7e0bc7490060a62b2db93b4e5bcac9d0275
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-03-16 21:19:31 +0530

    Implement common layout for the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>Header</header>
+        {children}
+        <footer>Footer</footer>
+      </body>
     </html>
   );
 }
