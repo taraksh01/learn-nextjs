f63da50ea7826ae7e08bd18b542e788f6c0f4c5e
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-03-17 10:17:52 +0530

    Implement dynamic metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,7 +1,15 @@
-export default function ProductDetailsPage({
-  params,
-}: {
-  params: { productId: string };
-}) {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export function generateMetadata({ params }: Props): Metadata {
+  return { title: `Product details ${params.productId}` };
+}
+
+export default function ProductDetailsPage({ params }: Props) {
   return <div>Product details page {params.productId}</div>;
 }
