7e9f9182ab4aade2dba4decdb07e3cb266ede7a4
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-03-19 20:17:36 +0530

    Implement middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if (themePreference) response.cookies.set("theme", "dark");
+
+  response.headers.set("custom-key", "custom-value");
+  return response;
+
+  if (request.nextUrl.pathname === "/profile")
+    return NextResponse.rewrite(new URL("/login", request.url));
+  // return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/profile", "/login"],
+};
